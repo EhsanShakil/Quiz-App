a06b8200b9b2250a3bf1173408c4595564ae1693
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-23 17:23:38 +0500

    making it progressive web app

---
+++ src/swDev.tsx
@@ -0,0 +1,13 @@
+export function swDev() {
+  let url = `${process.env.PUBLIC_URL}/sw.js`;
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register(url)
+      .then((response) => {
+        console.log("Service Worker registered.");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
